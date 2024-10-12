a8e5ed206a664c70dff62cbc406f4a30c60c8419
phil <p@pkpowell.net>
2024-10-12 19:08:05 +0200

    updates

--- runwait.go
+++ runwait.go
@@ -3,11 +3,12 @@ package groups
 import (
 	"context"
 	"fmt"
-	"runtime"
 	"time"
 )
 
 type Function func()
+type FunctionErr func() error
+
 type Options struct {
 	Timeout time.Duration
 	Ctx     context.Context
@@ -23,6 +24,9 @@ var opts = Options{
 	// Cancel:  cancel,
 }
 
+// RunWait executes the provided functions concurrently and waits for them all to complete.
+// The functions are executed in separate goroutines, and a sync.WaitGroup is used to
+// wait for all functions to finish before returning.
 func RunWait(functions []Function, Opts *Options) {
 	waitChan := make(chan struct{}, len(functions))
 	length := len(functions)
@@ -33,11 +37,9 @@ func RunWait(functions []Function, Opts *Options) {
 	} else {
 		Opts.Ctx, Opts.cancel = context.WithTimeout(Opts.Ctx, to)
 	}
-	// ctx, cancel := context.WithTimeout(Opts.Ctx, to)
 
 	for _, fu := range functions {
 		go func(f Function) {
-			// fmt.Printf("Starting job #%d\n", idx)
 			f()
 			waitChan <- struct{}{}
 		}(fu)
@@ -51,7 +53,7 @@ func RunWait(functions []Function, Opts *Options) {
 			return
 		case <-waitChan:
 			length--
-			// fmt.Printf("length = %d\n", length)
+
 			if length == 0 {
 				fmt.Printf("All %d jobs done\n", length)
 				if Opts.Ctx.Err() != nil {
@@ -59,22 +61,66 @@ func RunWait(functions []Function, Opts *Options) {
 				}
 				Opts.cancel()
 				return
-
-				// } else {
-				// 	fmt.Printf("Still waiting...\n")
 			}
 		}
 	}
 }
 
-func CallerName(skip int) string {
-	pc, _, _, ok := runtime.Caller(skip + 1)
-	if !ok {
-		return ""
+// RunWaitErr executes the provided functions concurrently and waits for them all to complete.
+// The functions are executed in separate goroutines, and a sync.WaitGroup is used to
+// wait for all functions to finish before returning.
+func RunWaitErr(functions []FunctionErr, Opts *Options) {
+	waitChan := make(chan struct{}, len(functions))
+	length := len(functions)
+	to := Opts.Timeout
+	fmt.Printf("Starting %d jobs. Timeout = %s\n", length, to.String())
+	if Opts.Ctx == nil {
+		Opts.Ctx, Opts.cancel = context.WithTimeout(context.Background(), to)
+	} else {
+		Opts.Ctx, Opts.cancel = context.WithTimeout(Opts.Ctx, to)
 	}
-	f := runtime.FuncForPC(pc)
-	if f == nil {
-		return ""
+	// ctx, cancel := context.WithTimeout(Opts.Ctx, to)
+	var errGroup error
+
+	for _, fu := range functions {
+		go func(f FunctionErr) {
+			// fmt.Printf("Starting job #%d\n", idx)
+			err := f()
+			if err != nil {
+				errGroup = fmt.Errorf("%s %w", errGroup, err)
+			}
+			waitChan <- struct{}{}
+		}(fu)
 	}
-	return f.Name()
+
+	for {
+		select {
+		case <-Opts.Ctx.Done():
+			fmt.Printf("Canceling jobs %s\n", Opts.Ctx.Err())
+			Opts.cancel()
+			return
+		case <-waitChan:
+			length--
+			if length == 0 {
+				fmt.Printf("All %d jobs done\n", length)
+				if errGroup != nil {
+					fmt.Printf("Errors encountered %s\n", errGroup)
+				}
+				Opts.cancel()
+				return
+			}
+		}
+	}
 }
+
+// func CallerName(skip int) string {
+// 	pc, _, _, ok := runtime.Caller(skip + 1)
+// 	if !ok {
+// 		return ""
+// 	}
+// 	f := runtime.FuncForPC(pc)
+// 	if f == nil {
+// 		return ""
+// 	}
+// 	return f.Name()
+// }
